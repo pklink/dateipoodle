5fe1890ff08aedb412bf77deab586645e0d83917
pierre <pierre+git@einself.net>
2020-08-17 01:03:01 +0200

    add test for `POST /files`

--- src/test/java/net/einself/dateipoodle/NativePudelResourceIT.java
+++
@@ -1,9 +0,0 @@
-package net.einself.dateipoodle;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativePudelResourceIT extends UploadResourceTest {
-
-    // Execute the same tests but in native mode.
-}
--- src/test/java/net/einself/dateipoodle/UploadResourceTest.java
+++ src/test/java/net/einself/dateipoodle/UploadResourceTest.java
@@ -1,21 +1,79 @@
 package net.einself.dateipoodle;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import net.einself.dateipoodle.dto.UploadFileRequest;
+import net.einself.dateipoodle.service.FileService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.ws.rs.core.MediaType;
+import java.io.File;
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 
 @QuarkusTest
 public class UploadResourceTest {
 
+    @InjectMock
+    FileService fileService;
+
     @Test
-    public void testHelloEndpoint() {
+    public void testOk() throws IOException {
+        // given
+        final var file = new net.einself.dateipoodle.domain.File();
+        file.id = "foo";
+        file.name = "bar.jpg";
+
+        // given
+        final var tmpFile = File.createTempFile("dateipoodle", "test");
+        tmpFile.deleteOnExit();
+
+        // when
+        Mockito.when(fileService.create(any(UploadFileRequest.class))).thenReturn(file);
+
         given()
-          .when().get("/dateipoodle")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+            .multiPart("file", tmpFile)
+            .multiPart("fileName", "test.jpg")
+            .when().post("/files")
+            .then()
+            .statusCode(200)
+            .body("id", is("foo"))
+            .body("name", is("bar.jpg"));
+    }
+
+    @Test
+    public void test400_emptyForm() {
+        given()
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .when().post("/files")
+            .then()
+            .statusCode(400);
+    }
+
+    @Test
+    public void test400_file() {
+        given()
+            .multiPart("fileName", "test.jpg")
+            .when().post("/files")
+            .then()
+            .statusCode(400);
+    }
+
+    @Test
+    public void test400_fileName() throws IOException {
+        // given
+        final var tmpFile = File.createTempFile("dateipoodle", "test");
+        tmpFile.deleteOnExit();
+
+        given()
+            .multiPart("file", tmpFile)
+            .when().post("/files")
+            .then()
+            .statusCode(400);
     }
 
 }
