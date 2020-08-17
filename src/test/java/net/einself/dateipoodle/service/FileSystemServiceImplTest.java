3b527fe1b883c486b52046dce9c092d1da7dc204
pierre <pierre+git@einself.net>
2020-08-17 20:36:59 +0200

    add FileSystemServiceImplTest

--- src/test/java/net/einself/dateipoodle/service/FileSystemServiceImplTest.java
+++ src/test/java/net/einself/dateipoodle/service/FileSystemServiceImplTest.java
@@ -1,26 +1,67 @@
 package net.einself.dateipoodle.service;
 
 import net.einself.dateipoodle.config.StorageConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 class FileSystemServiceImplTest {
 
     private FileSystemServiceImpl underTest;
 
     StorageConfig storageConfig;
+    String tmpDir;
 
-    public void init() {
+    @BeforeEach
+    public void init() throws IOException {
+        // given
         storageConfig = Mockito.mock(StorageConfig.class);
+
+        // given
+        tmpDir = Files.createTempDirectory("dateipoodle-test").toString();
+
+        // given
+        Mockito.when(storageConfig.getPath()).thenReturn(tmpDir);
+
         underTest = new FileSystemServiceImpl(storageConfig);
     }
 
-    //@Test
-    void testDelete_exists() {
+    @Test
+    void testDelete_exists() throws IOException {
+        // given
+        final var file = Files.createFile(Paths.get(tmpDir, "dateipoodle-test.delete-exists"));
 
+        // when
+        underTest.delete("dateipoodle-test.delete-exists");
+
+        // then
+        Assertions.assertTrue(Files.notExists(Paths.get(tmpDir, "dateipoodle.test")));
     }
 
-    //@Test
-    void store() {
+    @Test
+    void testDelete_notExists() {
+        // when
+        underTest.delete("dateipoodle-test.delete-not-exists");
+
+        // then
+        Assertions.assertTrue(Files.notExists(Paths.get(tmpDir, "dateipoodle-notexists.test")));
+    }
+
+    @Test
+    void store() throws IOException {
+        // given
+        final var file = Files.createTempFile("dateipoodle-test", "store.src").toFile();
+
+        // when
+        underTest.store(file, "dateipoodle-test.store.dst");
+
+        // then
+        Files.exists(Paths.get(tmpDir, "dateipoodle-test.store.dst"));
     }
 
 }
